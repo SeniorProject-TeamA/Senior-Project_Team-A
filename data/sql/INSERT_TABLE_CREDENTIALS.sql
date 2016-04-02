c449fcc12564e5e593dadce17baa3ff8fc0476e3
TeamA <project.emaila@gmail.com>
2016-04-02 13:35:14 -0500

    Upload new SQL injection and test scripts

---
+++ data/sql/INSERT_TABLE_CREDENTIALS.sql
@@ -0,0 +1,37 @@
+INSERT INTO credentials 
+VALUES ('1', 'Provo12');
+
+INSERT INTO credentials 
+VALUES ('2', 'Marvel02');
+
+INSERT INTO credentials 
+VALUES ('3', 'NoisyLotus1');
+
+INSERT INTO credentials 
+VALUES ('4', 'Typical11');
+
+INSERT INTO credentials 
+VALUES ('5', 'pAradise77');
+
+INSERT INTO credentials 
+VALUES ('6', 'taranTula1965');
+
+INSERT INTO credentials 
+VALUES ('7', 'mo22erellaM3lt');
+
+INSERT INTO credentials 
+VALUES ('8', '3ggPlants');
+
+INSERT INTO credentials 
+VALUES ('9', 'Charg3rsRul3');
+
+INSERT INTO credentials 
+VALUES ('10', 'Silv3rAndBlack');
+
+INSERT INTO credentials 
+VALUES ('11', 'pretzy1');
+
+INSERT INTO credentials 
+VALUES ('12', 'Gh0st1y');
+
+
