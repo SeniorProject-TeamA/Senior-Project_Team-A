270859a85556303a358081f981db8568beceb050
TeamA <justin@byrne-systems.com>
2016-04-03 08:49:02 -0700

    Uploading new Logic and UI

---
+++ lib/wsc_Framework/bootstrap.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * WSC DESCRIPTION!
+ *
+ * @package     WSC
+ * @author      Justin Don Byrne <justinbyrne001@gmail.com>
+ * @coauthor    James Coltman <email@address.com>
+ * @version     0.1 [In development]
+ * @copyright   2016
+ */
+
+
+
+?>
